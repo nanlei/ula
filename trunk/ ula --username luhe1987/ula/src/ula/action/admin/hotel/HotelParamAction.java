e6832ceb547662986754238af891bf02d923f1ae
nanlei1987 <nanlei1987@2038cbb2-947a-5d98-a095-d0740f98ae6c>
2011-03-01 15:01:37 +0000

    add hotel function - java page

--- trunk/ ula --username luhe1987/ula/src/ula/action/admin/hotel/HotelParamAction.java
+++ trunk/ ula --username luhe1987/ula/src/ula/action/admin/hotel/HotelParamAction.java
@@ -24,28 +24,59 @@ public class HotelParamAction extends CommonAction {
 		return hotelParam;
 	}
 
+	/**
+	 * 酒店参数列表
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public String hotelparam() throws Exception {
 		hotelParams = getServiceManager().getHotelService().getAllHotelParams();
 		return "hotelparam";
 	}
 
+	/**
+	 * 添加参数
+	 * 
+	 * @return
+	 */
 	public String addParam() {
 		try {
-			getServiceManager().getHotelService().addHotelParam(
+			// 检查参数名称是否已经存在
+			int flag = getServiceManager().getHotelService().getCheckNameExist(
 					getParametersAsMap());
-			this.setAlertMessage(AlertMessage.HOTEL_PARAM_ADD_SUCCESS);
-			return SUCCESS;
+			if (flag > 0) {
+				this.setAlertMessage(AlertMessage.HOTEL_PARAM_NAME_EXIST);
+				return SUCCESS;
+			} else {
+				getServiceManager().getHotelService().addHotelParam(
+						getParametersAsMap());
+				this.setAlertMessage(AlertMessage.HOTEL_PARAM_ADD_SUCCESS);
+				return SUCCESS;
+			}
 		} catch (Exception e) {
+			this.setAlertMessage(AlertMessage.HOTEL_PARAM_ADD_FAILURE);
 			return ERROR;
 		}
 	}
 
+	/**
+	 * 获取参数信息供更新
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public String hotelParamPreUpdate() throws Exception {
 		hotelParam = getServiceManager().getHotelService().getHotelParamById(
 				getParametersAsMap());
 		return "hotelParamPreUpdate";
 	}
 
+	/**
+	 * 更新参数
+	 * 
+	 * @return
+	 */
 	public String updateParam() {
 		try {
 			getServiceManager().getHotelService().updateHotelParam(
@@ -53,17 +84,24 @@ public class HotelParamAction extends CommonAction {
 			this.setAlertMessage(AlertMessage.HOTEL_PARAM_UPDATE_SUCCESS);
 			return SUCCESS;
 		} catch (Exception e) {
+			this.setAlertMessage(AlertMessage.HOTEL_PARAM_UPDATE_FAILURE);
 			return ERROR;
 		}
 	}
 
+	/**
+	 * 删除参数
+	 * 
+	 * @return
+	 */
 	public String hotelParamDelete() {
 		try {
 			getServiceManager().getHotelService().deleteHotelParam(
 					getParametersAsMap());
-			this.setAlertMessage(AlertMessage.HOTEL_PARAM_UPDATE_SUCCESS);
+			this.setAlertMessage(AlertMessage.HOTL_PARAM_DELETE_SUCCESS);
 			return SUCCESS;
 		} catch (Exception e) {
+			this.setAlertMessage(AlertMessage.HOTL_PARAM_DELETE_FAILURE);
 			return ERROR;
 		}
 	}
